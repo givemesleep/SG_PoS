eca3f19fee3cacef843b2acc7decdd5fb615a2ae
Justice Bellen <74264301+givemesleep@users.noreply.github.com>
2023-11-05 22:51:31 +0800

    Add files via upload
    Trial Connection

---
+++ frmtestcon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SGpos.LogIn
+{
+    public partial class frmtestcon : Form
+    {
+        static string server = "localhost";
+        static string dbase = "dbusers;";
+        static string uid = "root";
+        static string pass = ";";
+        static string all = $"server={server}; database={dbase}; Uid={uid}; password={pass};";
+        public frmtestcon()
+        {
+            InitializeComponent();
+            lblfa.Visible = false;
+            lblsu.Visible = false;
+        }
+
+        private void lblfa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void G_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection(all);
+
+            try
+            {
+                con.Open();
+                lblsu.Visible = true;
+                lblfa.Visible = false;
+            }
+            catch
+            {
+                con.Open();
+                lblfa.Visible = true;
+                lblsu.Visible = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
